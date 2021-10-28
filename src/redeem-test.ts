114512d6f9503164718ab66d34b48d740d35f62d
Rosca Alexandru <alex.rosca1996@gmail.com>
2021-10-28 19:51:29 +0300

    fix typo

--- src/redeem-test.ts
+++ src/redeem-test.ts
@@ -2,7 +2,7 @@ import { connection, walletSinger } from './common-setup';
 import auctionView from './mock/redeem/auctionViewItems';
 import { sendRedeemBid } from './web/actions/sendRedeemBid';
 
-const payingAccount = 'payingAccount';
+const payingAccount = '9AVaowib8ePah1VdJft6mgZtYQcHgLA4y1TAEV22Jhan';
 
 export const redeemAuction = async () => {
   const result = await sendRedeemBid(
