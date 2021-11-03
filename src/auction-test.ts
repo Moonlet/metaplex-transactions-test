60237771f74bad96b9f2a62a1f897d72aab60d71
Rosca Alexandru <alex.rosca1996@gmail.com>
2021-11-03 22:53:58 +0200

    remove unsed code

--- src/auction-test.ts
+++ src/auction-test.ts
@@ -1,49 +1,13 @@
-import { Transaction } from '@solana/web3.js';
-import {
-  getRentExemptions,
-  MasterEditionV1,
-  MasterEditionV2,
-  ParsedAccount,
-  SafetyDepositDraft,
-  StringPublicKey,
-} from './blockchain';
+import { getRentExemptions } from './blockchain';
 import { createAuctionManager } from './blockchain/operations/createAuction';
 import { connection, walletSinger } from './common-setup';
 import auctionSettingsInstantSale from './mock/auction/auctionSettingsInstantSale';
 import safetyDepositDraft from './mock/auction/safetyDepositDrafts';
 import whitelistedCreatorsByCreator from './mock/auction/whitelistedCreatorsByCreator';
-import { masterEdition } from './mock/redeem/auctionViewItems';
 import { buildAuctionSettings } from './ui/auction-settings';
 import { SaleType } from './ui/types';
 
-// todo: get from DB: {pubkey: "67kGFRDHRWHhxJzuVXpZivkRyTJFUBAfeaAxfdNWgy3J"}
-const getMasterEditionAccount = async (
-  pubkey: StringPublicKey
-): Promise<ParsedAccount<MasterEditionV1 | MasterEditionV2>> => {
-  return {
-    pubkey,
-    info: masterEdition,
-  };
-};
-
-const initSafetyDeposit = async (): Promise<SafetyDepositDraft> => {
-  const localSafetyDraft: SafetyDepositDraft = { ...safetyDepositDraft };
-
-  await localSafetyDraft.metadata.info.init(); // initialize edition & masterEdition fields
-  const masterEdition = localSafetyDraft.metadata.info.masterEdition;
-  if (masterEdition) {
-    localSafetyDraft.masterEdition = await getMasterEditionAccount(
-      masterEdition
-    );
-    return localSafetyDraft;
-  }
-  throw new Error('Missing master edition');
-};
-
 export const triggerAuction = async () => {
-  const safetyDeposit = await initSafetyDeposit();
-  // console.log(safetyDeposit);
-
   const auctionSettingsBuidler = buildAuctionSettings(SaleType.Auction, 0.01, {
     saleEnds: 1,
     tickSize: 0.01,
@@ -60,14 +24,13 @@ export const triggerAuction = async () => {
     rentExemption,
     whitelistedCreatorsByCreator, // this is a kind of cache, if missing => getWhitelistedCreator(creator: StringPublicKey) will be called
     auctionSettingsBuidler,
-    safetyDeposit
+    safetyDepositDraft
   );
 
   console.log(result);
 };
 
 export const triggerAuctionInstantSale = async () => {
-  const safetyDeposit = await initSafetyDeposit();
   const rentExemption = await getRentExemptions(connection);
 
   const result = await createAuctionManager(
@@ -75,10 +38,10 @@ export const triggerAuctionInstantSale = async () => {
     rentExemption,
     whitelistedCreatorsByCreator,
     auctionSettingsInstantSale,
-    safetyDeposit
+    safetyDepositDraft
   );
 
   console.log(result);
 };
 
-triggerAuction();
+triggerAuctionInstantSale();
